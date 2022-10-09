e97981ea22158c8cf9f5c145e247f7bb3f36111a
Chivan9406 <ivan9406@hotmail.com>
2022-10-08 19:21:25 -0500

    validacion formulario

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
 const modalImagen = document.querySelector('#modal-imagen')
 
-modalImagen.addEventListener('show.bs.modal', (e) => {
-    const enlace = e.relatedTarget
-    const rutaImagen = enlace.getAttribute('data-bs-imagen')
+if (modalImagen) {
+    modalImagen.addEventListener('show.bs.modal', (e) => {
+        const enlace = e.relatedTarget
+        const rutaImagen = enlace.getAttribute('data-bs-imagen')
 
-    // Construir imagen
-    const imagen = document.createElement('img')
-    imagen.src = `img/${rutaImagen}.jpg`
-    imagen.classList.add('img-fluid')
-    imagen.alt = 'imagen galería'
+        // Construir imagen
+        const imagen = document.createElement('img')
+        imagen.src = `img/${rutaImagen}.jpg`
+        imagen.classList.add('img-fluid')
+        imagen.alt = 'imagen galería'
 
-    // Asignar al modal
-    const contenidoModal = document.querySelector('.modal-body')
-    contenidoModal.appendChild(imagen)
-})
+        // Asignar al modal
+        const contenidoModal = document.querySelector('.modal-body')
+        contenidoModal.appendChild(imagen)
+    })
 
-modalImagen.addEventListener('hidden.bs.modal', () => {
-    const contenidoModal = document.querySelector('.modal-body')
-    contenidoModal.firstChild.remove()
-})
+    modalImagen.addEventListener('hidden.bs.modal', () => {
+        const contenidoModal = document.querySelector('.modal-body')
+        contenidoModal.firstChild.remove()
+    })
+}
+
+// Validation
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
